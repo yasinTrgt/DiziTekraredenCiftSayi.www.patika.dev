2445b98e493290dd7b3f60090ea8a0dc33a5ced1
yasin turgut <yasin@yasins-MacBook-Air.local>
2023-02-20 17:23:36 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    static boolean isFind(int[]arr ,int value){
+        for(int i: arr){
+            if(i==value){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[]args) {
+        int[] list = {2, 4, 456, 3, 9, 88, 88, 3, 3, 75, 56, 2};
+        int[] duplicate = new int[list.length];
+        int startIndex = 0;
+        for (int i = 0; i < list.length; i++) {
+            for (int j = 0; j < list.length; j++) {
+                if (i != j && (list[i] == list[j])) {
+                    if (!isFind(duplicate, list[i])) {
+                        duplicate[startIndex++] = list[i];
+                    }
+                    break;
+                }
+            }
+        }
+        for (int value : duplicate) {
+            if (value != 0) {
+                System.out.print(value + ",");
+            }
+        }
+    }
+}
